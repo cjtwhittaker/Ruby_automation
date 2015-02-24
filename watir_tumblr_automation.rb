069dcb2d83594b9a1270f68dd396d07a53676cac
Charlie Whittaker <cwhittaker@spartaglobal.com>
2015-02-24 16:40:51 +0000

    added confirmation checker and massage

--- watir_tumblr_automation.rb
+++ watir_tumblr_automation.rb
@@ -1,5 +1,5 @@
 require 'watir'
-bolg_add = 'http://choppedpizzaluminaryblr.tumblr.com/'
+bolg_address = 'http://choppedpizzaluminaryblr.tumblr.com/'
 email = 'edghuj@gmail.com'
 pass = '147852369'
 title = 'Lorem ipsum dolor'
@@ -12,10 +12,23 @@ browser.goto 'http://tumblr.com/login'
 browser.text_field(id: 'signup_email').set email
 browser.text_field(id: 'signup_password').set pass
 browser.button(id: 'signup_forms_submit').click
-browser.i(class: 'icon_post_text').click
 
 #making the post
+browser.i(class: 'icon_post_text').click
 title = browser.element(class: 'editor-plaintext').send_keys title
 text_post = browser.element(class: 'editor-richtext').send_keys post_text
 hashtag = browser.elements(class: 'editor-plaintext')[2].send_keys hashtags
 post_button = browser.button(text: 'Post').click
+#checking that the post appears on the blog
+
+sleep 3 #should probably find a way to get it to work without this, all other wait methods seem to cause it to go to the new page too quickly though
+
+
+
+
+browser.goto bolg_address
+if browser.element(class: 'body-text').text == post_text
+  puts 'the post has been submitted successfully'
+else
+  puts 'boooooo, seems like there was an error posting your blog :('
+end
