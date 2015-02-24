ebf0775855f4960a37f2fe374995de21d7983366
Charlie Whittaker <cwhittaker@spartaglobal.com>
2015-02-24 15:19:56 +0000

    refactored code on watir tumblr

--- watir_tumblr_automation.rb
+++ watir_tumblr_automation.rb
@@ -15,17 +15,12 @@ pass_ent.set pass
 login_button = browser.button id: 'signup_forms_submit'
 login_button.click
 
-text_post_icon = browser.i class: 'icon_post_text'
-text_post_icon.click
+browser.i(class: 'icon_post_text').click
 
-title = browser.element class: 'editor-plaintext'
-title.send_keys 'Lorem ipsum dolor.'
+title = browser.element(class: 'editor-plaintext').send_keys 'Lorem ipsum dolor.'
 
-text_post = browser.element class: 'editor-richtext'
-text_post.send_keys 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quo laboriosam architecto sed minus odio quia quis dignissimos sit asperiores reprehenderit cupiditate exercitationem temporibus tempora quibusdam ipsa at, quidem voluptas. Laboriosam.'
+text_post = browser.element(class: 'editor-richtext').send_keys 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quo laboriosam architecto sed minus odio quia quis dignissimos sit asperiores reprehenderit cupiditate exercitationem temporibus tempora quibusdam ipsa at, quidem voluptas. Laboriosam.'
 
-hashtag = browser.elements(class: 'editor-plaintext')[2]
-hashtag.send_keys '#Lorem #ipsum #dolor'
+hashtag = browser.elements(class: 'editor-plaintext')[2].send_keys '#Lorem #ipsum #dolor'
 
-post_button = browser.button text: 'Post'
-post_button.click
+post_button = browser.button(text: 'Post').click
