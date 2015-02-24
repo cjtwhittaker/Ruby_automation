78c22acad611206fde54c370b9b619f864331124
Charlie Whittaker <cwhittaker@spartaglobal.com>
2015-02-24 15:24:09 +0000

    refactored more code on watir tumblr

--- watir_tumblr_automation.rb
+++ watir_tumblr_automation.rb
@@ -1,26 +1,21 @@
 require 'watir'
-
+bolg_add = 'http://choppedpizzaluminaryblr.tumblr.com/'
 email = 'edghuj@gmail.com'
 pass = '147852369'
+title = 'Lorem ipsum dolor'
+post_text = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quo laboriosam architecto sed minus odio quia quis dignissimos sit asperiores reprehenderit cupiditate exercitationem temporibus tempora quibusdam ipsa at, quidem voluptas. Laboriosam.'
+hashtags = '#Lorem #ipsum #dolor'
 
 browser = Watir::Browser.new :ff
 browser.goto 'http://tumblr.com/login'
 
-email_ent = browser.text_field id: 'signup_email'
-email_ent.set email
-
-pass_ent = browser.text_field id: 'signup_password'
-pass_ent.set pass
-
-login_button = browser.button id: 'signup_forms_submit'
-login_button.click
-
+browser.text_field(id: 'signup_email').set email
+browser.text_field(id: 'signup_password').set pass
+browser.button(id: 'signup_forms_submit').click
 browser.i(class: 'icon_post_text').click
 
-title = browser.element(class: 'editor-plaintext').send_keys 'Lorem ipsum dolor.'
-
-text_post = browser.element(class: 'editor-richtext').send_keys 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quo laboriosam architecto sed minus odio quia quis dignissimos sit asperiores reprehenderit cupiditate exercitationem temporibus tempora quibusdam ipsa at, quidem voluptas. Laboriosam.'
-
-hashtag = browser.elements(class: 'editor-plaintext')[2].send_keys '#Lorem #ipsum #dolor'
-
+#making the post
+title = browser.element(class: 'editor-plaintext').send_keys title
+text_post = browser.element(class: 'editor-richtext').send_keys post_text
+hashtag = browser.elements(class: 'editor-plaintext')[2].send_keys hashtags
 post_button = browser.button(text: 'Post').click
